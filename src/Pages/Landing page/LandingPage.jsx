25485355ed07f6f8de39f2eb100df5c1677646fb
Muhammad Huzefa Javed <Huzefahjaved@gmail.com>
2025-02-16 21:28:37 +0500

    short the code

--- src/Pages/Landing page/LandingPage.jsx
+++ src/Pages/Landing page/LandingPage.jsx
@@ -4,8 +4,23 @@ import { ProductsCategories } from "./LandingPageSections/ProductsCategories"
 
 export const Landingpage =()=> {
     return (<>
-        <HeroSection />
+        <HeroSection 
+           bg_link=""
+            heading="Elevate Your Shopping Experience"
+             para="Shop premium products at unbeatable prices with fast shipping and secure payments."
+              button_text="Shop Now" />
         <ProductsCategories/>
-        <JacketSection/>
+       <HeroSection
+         bg_link="https://blackdiamond-cms.zaneray.com/cms/images/Z5qWIpbqstJ9-A_x_Access_Down_Hoody_Womens_Homepage_Hero_2880x1620px.jpg?auto=format%2Ccompress&w=2400&h=1350"
+        heading="Everyday Down"
+        para="The all new Access Down 1.0"
+        button_text="shop now"
+         />
+         <HeroSection
+         bg_link="https://blackdiamond-cms.zaneray.com/cms/images/ZsZXG0aF0TcGJMbK_S24-Hydra-Homepage-Hero-2880x1620px.jpg?auto=format%2Ccompress&w=2400&h=1350"
+         heading="The Hydra Ice Tool"
+         para="The Many-Headed Beast"  
+         button_text="Shop Now"
+         />
     </>)
 }
