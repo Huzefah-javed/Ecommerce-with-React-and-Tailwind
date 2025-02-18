5fc60f83a8cb356cccaf39808b67473677cb1f50
Muhammad Huzefa Javed <Huzefahjaved@gmail.com>
2025-02-18 21:09:19 +0500

    Add menu bar detail component 100%

--- src/Pages/Landing page/LandingPageSections/HeroSection.jsx
+++ src/Pages/Landing page/LandingPageSections/HeroSection.jsx
@@ -4,10 +4,10 @@ import { MainButton } from "../../../web Components/Buttons/MainButton"
 
 export const HeroSection =({bg_link, heading, para, button_text,})=> {
     return <>
-       <div style={{ backgroundImage: `url(${bg_link})` }} className={`h-screen w-full relative flex justify-center flex-col bg-cover bg-center before:absolute before:z-10 before:h-full before:w-full before:bg-black before:opacity-50`}>
+       <div style={{ backgroundImage: `url(${bg_link})` }} className={`h-screen w-full relative z-10 flex justify-center flex-col bg-cover bg-center before:absolute before:z-20 before:h-full before:w-full before:bg-black before:opacity-50`}>
         <div className="flex flex-col mt-10 ml-10 max-w-2xl">
-            <h1 className="text-6xl font-extrabold font-sans text-white relative z-40">{heading}</h1>
-            <p className="text-2xl font-medium font-sans pt-10 pb-10 text-white relative z-40">{para}</p>
+            <h1 className="text-6xl font-extrabold font-sans text-white relative z-20">{heading}</h1>
+            <p className="text-2xl font-medium font-sans pt-10 pb-10 text-white relative z-20">{para}</p>
             <MainButton text={button_text}/>
         </div>
 
--- src/web Components/Header/HeadermenuBarDetail.jsx
+++ src/web Components/Header/HeadermenuBarDetail.jsx
@@ -2,14 +2,19 @@ import { GrClose } from "react-icons/gr";
 import { MainButton } from "../../../src/web Components/Buttons/MainButton"
 
 
-export const HeaderMenubarDetail=()=> {
-    return <div className="fixed -top-0 h-dvh w-dvw bg-white p-8 z-[99]">
-        <header className=" w-full flex justify-between items-center">
+export const HeaderMenubarDetail=({menuActive, setMenuActive})=> {
+
+    const handleCloseMenu =()=> {
+        setMenuActive(false)
+    }
+    
+    return <div className={`fixed -top-0 left-0 h-dvh w-dvw bg-white p-8 transition-all duration-300 ease-in-out${menuActive? "visible opacity-100 z-50 ": "opacity-0 invisible -z-50"} overflow-y-scroll`}>
+        <header className=" w-full h-24 flex justify-between items-center fixed top-0 bg-white">
         <svg data-v-2e58fd37="" data-v-549ed7fb="" viewBox="0 0 1856 80" xmlns="http://www.w3.org/2000/svg" focusable="false" class="logo-link dark"><path data-v-2e58fd37="" d="M27.7372067,3.17560999e-14 L1.5242928e-13,39.9106145 L27.8078212,79.8167598 L27.8082682,79.8167598 L36.8223464,66.8487151 L17.7899441,39.630838 L36.5448045,12.6319553 L27.7372067,3.17560999e-14 Z M26.633743,39.9106145 L41.1343017,60.6458101 L55.5521788,39.9110615 L55.5521788,39.9097207 L41.070838,19.1293855 L26.633743,39.9106145 Z" id="Fill-1" fill="#231F20"></path></svg>
 
-        <button className=" h-fit w-fit text-[1.4rem] box-shadow p-2 cursor-pointer"><GrClose /></button>
+        <button onClick={()=>handleCloseMenu()} className=" h-fit w-fit text-[1.4rem] box-shadow p-2 cursor-pointer mr-10"><GrClose /></button>
         </header>
-        <div className="main-container flex justify-start overflow-y-scroll">
+        <div className={`main-container flex justify-start transition-all duration-500 ease-in-out ${menuActive? "mt-16 opacity-100": "mt-24 opacity-0"}`}>
             <div className="login-detail flex flex-col m-8 w-[24rem]">
                 <div className="mb-8 text-[1.5rem] capitalize tracking-tighter font-semibold">Login to your account</div>
                 <div className="email mb-8 flex flex-col">
@@ -18,7 +23,7 @@ export const HeaderMenubarDetail=()=> {
                 </div>
                 <div className="password mb-8 flex flex-col">
                     <label className="text-[0.89rem] text-zinc-700 mb-1" htmlFor="password">password*</label>
-                    <input className=" border-[0.5px] border-black border-solid p-3" type="password" name="password" id="email" />
+                    <input className=" border-[0.5px] border-black border-solid p-3" type="password" name="password" id="password" />
                 </div>
                 <div className="checkBox mb-8">
                     <input className="size-4 " type="checkbox" name="checkbox" id="checkbox" />
