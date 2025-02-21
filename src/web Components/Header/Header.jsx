7594891143f9a2d6478991792fcd062f4013368b
Muhammad Huzefa Javed <Huzefahjaved@gmail.com>
2025-02-21 22:37:03 +0500

    experience menu detail done

--- src/web Components/Header/Header.jsx
+++ src/web Components/Header/Header.jsx
@@ -12,6 +12,7 @@ export const Header =()=> {
     const [menuActive , setMenuActive] = useState(false)
     const [searchMenuActive , setSearchMenuActive] = useState(false)
     const [shopMenuActive , setShopMenuActive] = useState(false)
+    const [experienceMenu , setExperienceMenu] = useState(false)
 
    const handleMenuActive =()=> {
         setMenuActive(true)
@@ -22,6 +23,9 @@ export const Header =()=> {
    const handleShopMenuActive =()=> {
     setShopMenuActive(true)
    }
+   const handleExperienceMenuActive =()=> {
+    setExperienceMenu(true)
+   }
 
     return ( 
     <header className="w-full flex justify-between items-center absolute z-30 top-0 mt-10">
@@ -30,7 +34,7 @@ export const Header =()=> {
 
         <div className="flex mr-12 font-sans relative">
             <button onClick={()=> handleShopMenuActive()} className="text-white text-[1.1rem] font-bold ml-10 uppercase cursor-pointer">Shop</button>
-            <button className="text-white text-[1.1rem] font-bold ml-10 uppercase cursor-pointer">Experience</button>
+            <button onClick={()=>handleExperienceMenuActive()} className="text-white text-[1.1rem] font-bold ml-10 uppercase cursor-pointer">Experience</button>
             <button onClick={()=> handleSearchMenuActive()} className="text-white text-[1.5rem] font-bold ml-10 cursor-pointer hover:text-amber-700 transition-all duration-500"><IoIosSearch /></button>
             <button  onClick={()=>handleMenuActive()} className="text-white text-[1.5rem] font-bold ml-10 cursor-pointer"><RxHamburgerMenu /></button>
             <button className="text-white text-[1.5rem] font-bold ml-10 cursor-pointer hover:text-amber-700 transition-all duration-500"><MdOutlineShoppingBag />
@@ -39,7 +43,7 @@ export const Header =()=> {
         <HeaderMenubarDetail menuActive={menuActive} setMenuActive={setMenuActive}/>
         <HeaderSearchMenuDetail searchMenuActive={searchMenuActive} setSearchMenuActive={setSearchMenuActive}/>
         <HeaderShopMenu shopMenuActive={shopMenuActive} setShopMenuActive={setShopMenuActive}/>
-        <HeaderExperienceDetail/>
+        <HeaderExperienceDetail experienceMenu={experienceMenu} setExperienceMenu={setExperienceMenu}/>
     </header>
 )
 }
--- src/web Components/Header/HeaderExperienceDetail.jsx
+++ src/web Components/Header/HeaderExperienceDetail.jsx
@@ -1,26 +1,80 @@
 import { useState } from "react"
 import { GrClose } from "react-icons/gr"
 
-export const HeaderExperienceDetail =()=> {
+export const HeaderExperienceDetail =({experienceMenu , setExperienceMenu})=> {
                 const [singleLinkIndex, SetSingleLinkIndex] = useState(0)
+                const handleCloseExperienceMenu =()=> {
+                    setExperienceMenu(false)
+                }
                 const hoverLink = ["who we are", "Athlete", "Ambassadors", "Access and Inclusions", "SustainAbility", "Find A Dealer", "BD retail store", "Hydra Demo Center"]
+                const hoverContent = [
+                    {
+                        HeadingText: "bd heritage",
+                        paraText: null,
+                        
+                    },{
+                        HeadingText: "Discover the bd team",
+                        paraText: null,
+                        
+                    },{
+                        HeadingText: "Weekend warrior who charged",
+                        paraText: null,
+                        
+                    },{
+                        HeadingText: "building community",
+                        paraText: null,
+                        
+                    },{
+                        HeadingText: "Corporate responsibility",
+                        paraText: "The goals in which goals are accomplished is as important as achieving them at all",
+                        
+                    },{
+                        HeadingText: "support your local shop",
+                        paraText: null
+                        
+                    },{
+                       HeadingText: "visit our storefront locations",
+                        paraText: "A List of BD retail store locations, hours and contact information"
+                    },{
+                        
+                        HeadingText: "Experience the hydra",
+                        paraText: "Demo the events and locations where you can test drive the hydra ice tool"
+                    }
+                ]
 
                 const handleHoverEffect =(id)=> {
                     SetSingleLinkIndex(id)
                 }
+                return   <div className={`fixed -top-0 left-0 h-dvh w-dvw p-8 bg-black transition-all duration-300 ease-in-out ${experienceMenu? "opacity-100 z-50 visible": "opacity-0 -z-50 invisible"} overflow-y-scroll`}>
+                        <div style={{background: "url('logo bg.png')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundClip:"center"}}>
 
-    return   <div className={`fixed -top-0 left-0 h-dvh w-dvw bg-black p-8 transition-all duration-300 ease-in-out visible opacity-100 z-50 overflow-y-scroll`}>
-                    <header className=" w-full h-24 flex justify-between items-center fixed top-0 bg-black pt-4">
+                    <header className=" w-full h-24 flex justify-between items-center fixed top-0 z-[9] bg-black pt-4">
                     <img className="w-10" src="logo-white.png" alt="logo" />
             
-                    <button className=" h-fit w-fit text-[1.4rem] box-shadow p-2 cursor-pointer mr-10 bg-white"><GrClose /></button>
+                    <button onClick={()=>handleCloseExperienceMenu()} className=" h-fit w-fit text-[1.4rem] box-shadow p-2 cursor-pointer mr-10 bg-white"><GrClose /></button>
                     </header>
-                    <h1 className={`text-2xl text-white capitalize font-bold font-sans transition-all duration-200 ease-in-out mt-20 mb-8`}>Community</h1>
-                    <div className="ml-8 relative">
-                            <div className={`w-0.5 h-4 bg-[#cd4c1d] absolute left-0 mt-[0.4rem] mb-[0.4rem]`} style={{ top:  `${singleLinkIndex * (singleLinkIndex ? 2.8 : 0)}rem`}}></div>
-                        {hoverLink.map((singleLink, index)=>{
-                            return (<div key={index} onMouseEnter={()=>handleHoverEffect(index)} className="group text-white cursor-pointer text-[1.2rem] capitalize tracking-tighter m-4 font-medium">{singleLink}</div>)
-                        })}
+                    <h1 className={`text-2xl text-white capitalize font-bold font-sans transition-all duration-200 ease-in-out ${experienceMenu? "mt-20": "mt-36"} mb-8`}>Community</h1>
+                    <div className="main-body flex">
+                    <div className="ml-8 relative w-fit">
+                         <div className={`w-0.5 h-4 bg-[#cd4c1d] absolute left-0 mt-[1.4rem] mb-[1.4rem]`} style={{ top:`${singleLinkIndex * (singleLinkIndex ? 2.8 : 0)}rem`}}></div>
+                                 {hoverLink.map((singleLink, index)=>{
+                                     return (<div key={index} onMouseEnter={()=>handleHoverEffect(index)} className="text-white cursor-pointer text-[1.2rem] capitalize tracking-tighter m-4 font-medium">{singleLink}</div>)
+                                    })}
+                    </div> 
+                      <div className="mainHover-content h-[22rem] w-[30rem] ml-40 relative overflow-hidden">
+                            {
+                                hoverContent.map((singleContent, index)=>{
+                                    return (<div key={index} className={`absolute top-0 left-0 h-[20rem] flex flex-col justify-evenly transition-all duration-[0.7s] ease-in-out${singleLinkIndex === index ? " opacity-100" : " top-[4rem] opacity-0 invisible"}`}>
+                                        <h1 className="text-6xl break-words uppercase text-white font-extrabold">{singleContent.HeadingText}</h1>
+                                        {singleContent.paraText? (<p className="font-sans text-white">{singleContent.paraText}</p>): ""}
+                                        <button className="p-2 border-white text-white border-4 border-solid w-20 font-extrabold cursor-pointer transition-all duration-500 ease-in-out origin-top-right hover:border-black">Explore</button>
+                                    </div>
+                                    )
+                                })
+                            }
+                      </div>
                     </div>
+                    <h1 className={`text-2xl text-white capitalize font-bold font-sans transition-all duration-200 ease-in-out mt-20 mb-8`}>Content</h1>
                     </div>
+                        </div>
 }
