c3bd3a772eb99eecb0e5897548dce471c7ea4442
Muhammad Huzefa Javed <Huzefahjaved@gmail.com>
2025-03-20 11:04:36 +0500

    loading structure is done

--- src/web Components/ProductsLoading.jsx
+++ src/web Components/ProductsLoading.jsx
@@ -1,7 +1,7 @@
 export const ProductLoading = ({productPage})=> {
     return(
         [...Array(6)].map((_, index)=>{
-            return(<div key={index} className={` h-fit flex flex-col ${productPage? "w-full": "md:w-64 w-56"} cursor-pointer border-[0.25px] border-[#f7f7f6] border-solid product-box-shadow `} >
+            return(<div key={index} className={` h-fit flex flex-col ${productPage? "w-full": "md:w-72 w-56"} cursor-pointer border-[0.25px] border-[#f7f7f6] border-solid product-box-shadow `} >
                     <div className="bg-[#d7d7d7] h-72 w-auto">
                     </div>
                     <div className="p-2">
